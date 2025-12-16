4ec24d05946f73d6178eabb177fee730a69bbd11
loki-isl <lokeshkumar.dhanapalan@inspirisys.com>
2025-12-16 12:52:19 +0530

    Updated index.html styles.css scripts.js

--- script.js
+++ script.js
@@ -97,4 +97,13 @@ document.addEventListener('DOMContentLoaded', function(){
   mobileToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
   });
+
+  const navToggle = document.getElementById("navToggle");
+  //const navLinks = document.querySelector(".nav-links");
+
+  navToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+
 });
